e197f2e21fa4d21f7314bd99376c31fe5c42ea91
bhhdev <bhh.develop@gmail.com>
2026-01-31 13:56:21 +0100

    feat(employee): implement employee dashboard

--- app/src/Controller/AStaff/Employee/Dashboard/EmployeeDashboardController.php
+++ app/src/Controller/AStaff/Employee/Dashboard/EmployeeDashboardController.php
@@ -12,8 +12,32 @@ class EmployeeDashboardController extends AbstractController
     #[Route('/', name: 'index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('a_staff/employee/dashboard/employee_dashboard/index.html.twig', [
-            'controller_name' => 'EmployeeDashboardController',
-        ]);
+        $covoiturages = [
+            [
+                'id' => 1,
+                'date' => new \DateTime('today'),
+                'driver' => 'Jean Dupont',
+                'note' => 5,
+            ],
+            [
+                'id' => 2,
+                'date' => new \DateTime('today'),
+                'driver' => 'Marie Martin',
+                'note' => 2,
+            ],
+            [
+                'id' => 3,
+                'date' => new \DateTime('today'),
+                'driver' => 'Paul Bernard',
+                'note' => 3,
+            ],
+        ];
+
+        return $this->render(
+            'a_staff/employee/dashboard/employee_dashboard/index.html.twig',
+            [
+                'covoiturages' => $covoiturages,
+            ]
+        );
     }
 }
