aad54c9291065dc3dea022f133a811f3d29b40a9
bhhdev <bhh.develop@gmail.com>
2026-02-10 16:39:00 +0100

    feat(user): define User entity and relations

--- app/src/Entity/User.php
+++ app/src/Entity/User.php
@@ -2,15 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[ORM\Entity]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -18,114 +16,89 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
-    private ?string $email = null;
+    #[ORM\Column(length: 180, unique: true)]
+    private string $email;
 
-    /**
-     * @var list<string> The user roles
-     */
     #[ORM\Column]
+    private string $password;
+
+    #[ORM\Column(type: 'json')]
     private array $roles = [];
 
-    /**
-     * @var string The hashed password
-     */
-    #[ORM\Column]
-    private ?string $password = null;
+    #[ORM\Column(length: 50)]
+    private string $firstname;
+
+    #[ORM\Column(length: 50)]
+    private string $lastname;
+
+    #[ORM\Column(length: 10)]
+    private string $phone;
 
     #[ORM\Column]
-    private bool $isVerified = false;
+    private int $credits = 0;
 
-    public function getId(): ?int
+    #[ORM\Column]
+    private bool $isPassenger = true;
+
+    #[ORM\Column]
+    private bool $isDriver = false;
+
+    #[ORM\Column]
+    private \DateTimeImmutable $createdAt;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Trip::class)]
+    private Collection $trips;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Booking::class)]
+    private Collection $bookings;
+
+    #[ORM\OneToMany(mappedBy: 'owner', targetEntity: Vehicle::class)]
+    private Collection $vehicles;
+
+    public function __construct()
     {
-        return $this->id;
+        $this->trips = new ArrayCollection();
+        $this->bookings = new ArrayCollection();
+        $this->vehicles = new ArrayCollection();
+        $this->createdAt = new \DateTimeImmutable();
+        $this->credits = rand(0, 1000);
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
+    public function getId(): ?int { return $this->id; }
 
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
+    public function getEmail(): string { return $this->email; }
+    public function setEmail(string $email): self { $this->email = $email; return $this; }
 
-        return $this;
-    }
+    public function getFirstname(): string { return $this->firstname; }
+    public function setFirstname(string $f): self { $this->firstname = $f; return $this; }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUserIdentifier(): string
-    {
-        return (string) $this->email;
-    }
+    public function getLastname(): string { return $this->lastname; }
+    public function setLastname(string $l): self { $this->lastname = $l; return $this; }
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+    public function getPhone(): string { return $this->phone; }
+    public function setPhone(string $p): self { $this->phone = $p; return $this; }
 
-        return array_unique($roles);
-    }
+    public function getCredits(): int { return $this->credits; }
+    public function setCredits(int $c): self { $this->credits = $c; return $this; }
 
-    /**
-     * @param list<string> $roles
-     */
-    public function setRoles(array $roles): static
-    {
-        $this->roles = $roles;
+    public function getIsPassenger(): bool { return $this->isPassenger; }
+    public function setIsPassenger(bool $b): self { $this->isPassenger = $b; return $this; }
 
-        return $this;
-    }
+    public function getIsDriver(): bool { return $this->isDriver; }
+    public function setIsDriver(bool $b): self { $this->isDriver = $b; return $this; }
 
-    /**
-     * @see PasswordAuthenticatedUserInterface
-     */
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
+    public function getUserIdentifier(): string { return $this->email; }
+    public function getRoles(): array { $roles = $this->roles; $roles[] = 'ROLE_CLIENT'; return array_unique($roles); }
+    public function getPassword(): string { return $this->password; }
+    public function setPassword(string $password): self { $this->password = $password; return $this; }
+    public function eraseCredentials(): void {}
 
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
-     */
-    public function __serialize(): array
-    {
-        $data = (array) $this;
-        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
-
-        return $data;
-    }
-
-    #[\Deprecated]
-    public function eraseCredentials(): void
-    {
-        // @deprecated, to be removed when upgrading to Symfony 8
-    }
-
-    public function isVerified(): bool
-    {
-        return $this->isVerified;
-    }
-
-    public function setIsVerified(bool $isVerified): static
-    {
-        $this->isVerified = $isVerified;
-
+    public function addVehicle(Vehicle $v): self {
+        if (!$this->vehicles->contains($v)) {
+            $this->vehicles->add($v);
+            $v->setOwner($this);
+            $this->isDriver = true;
+        }
         return $this;
     }
 }
