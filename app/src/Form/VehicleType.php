62611c85b2ebf87a31953d4efa979e1073b77b29
bhhdev <bhh.develop@gmail.com>
2026-03-09 13:46:16 +0100

    feat(vehicle): create VehicleType form

---
+++ app/src/Form/VehicleType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vehicle;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VehicleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+        ->add('brand', TextType::class)
+
+        ->add('model', TextType::class)
+
+        ->add('color', TextType::class)
+
+        ->add('energy', ChoiceType::class, [
+            'choices' => [
+                'fossile' => 'fossile',
+                'hybride' => 'hybride',
+                'elec' => 'elec',
+            ],
+            'expanded' => true,
+            'multiple' => false
+        ])
+
+        ->add('licenseNumber', TextType::class)
+
+        ->add('firstRegistrationDate', DateType::class, [
+            'widget' => 'single_text'
+        ])
+
+        ->add('numberSeats', IntegerType::class)
+
+        ->add('airConditionning', CheckboxType::class, [
+            'required' => false,
+            'label' => false
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Vehicle::class,
+        ]);
+    }
+}
